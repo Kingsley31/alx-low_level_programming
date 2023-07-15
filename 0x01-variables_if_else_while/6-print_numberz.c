511e71ac05ef2fe286bfdd8c924639dc7660a1c6
Kingsley <kingsleyemeka31@gmail.com>
2023-07-15 06:40:01 -0700

    Added 6-print_numberz.c source file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-        int s_digit = '0';
+	int s_digit = '0';
 
-        while (s_digit <= '9')
-        {
-                putchar(s_digit);
-                s_digit++;
-        }
-        s_digit = '\n';
-        putchar(s_digit);
-        return (0);
+	while (s_digit <= '9')
+	{
+		putchar(s_digit);
+		s_digit++;
+	}
+	s_digit = '\n';
+	putchar(s_digit);
+	return (0);
 }
