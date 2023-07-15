63c04974e63fde8cb311179f8726409471c117f3
Kingsley <kingsleyemeka31@gmail.com>
2023-07-15 06:28:01 -0700

    Added 5-print_numbers.c source file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main (void)
+{
+	int s_digit = 0;
+
+	while (s_digit <= 9)
+	{
+		putchar(s_digit);
+		s_digit++;
+	}
+	s_digit = '\n';
+	putchar(s_digit);
+	return (0);
+}
