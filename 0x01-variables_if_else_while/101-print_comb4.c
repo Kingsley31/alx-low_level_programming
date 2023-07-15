1f49d3f80460046996f39c2fd730b013991e40e0
Kingsley <kingsleyemeka31@gmail.com>
2023-07-15 08:48:57 -0700

    Added 101-print_comb4.c source file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int outer_num = '0';
+	int comma = ',';
+	int space = ' ';
+	int new_line = '\n';
+
+	while (outer_num <= '7')
+	{
+		int inner_outer_num = outer_num + 1;
+
+		while (inner_outer_num <= '8')
+		{
+			int inner_inner_num = inner_outer_num + 1;
+
+			while (inner_inner_num <= '9')
+			{
+				putchar(outer_num);
+				putchar(inner_outer_num);
+				putchar(inner_inner_num);
+				if (outer_num != '7' || inner_outer_num != '8' || inner_inner_num != '9')
+				{
+					putchar(comma);
+					putchar(space);
+				}
+			}
+
+		}
+	}
+	putchar(new_line);
+	return (0);
+}
