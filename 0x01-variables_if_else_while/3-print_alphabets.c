a3bd6dae92011a660512fdf0ea87d04e8eab705a
Kingsley <kingsleyemeka31@gmail.com>
2023-07-15 06:06:47 -0700

    Added 3-print_alphabets.c source file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+	}
+	c = 'A';
+	while (c <= 'Z')
+	{
+		puchchar(c);
+	}
+	c = '\n';
+	putchar(c);
+	return (0);
+}
