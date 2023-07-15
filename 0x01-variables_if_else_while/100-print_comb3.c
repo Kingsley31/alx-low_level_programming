e883666ff34efbe834ec3ddc4e24ec2039909a0a
Kingsley <kingsleyemeka31@gmail.com>
2023-07-15 08:24:17 -0700

    Added 100-print_comb3.c source file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int outer_num = '0';
+	int comma = ',';
+	int space = ' ';
+	int new_line = '\n';
+
+	while (outer_num < '9')
+	{
+		int inner_num = outer_num + 1;
+
+		while (inner_num <= '9')
+		{
+			putchar(outer_num);
+			putchar(inner_num);
+			if(outer_num != '8' && inner_num != '9')
+			{
+				putchar(comma);
+				putchar(space);
+			}
+			inner_num++;
+		}
+		outer_num++;
+	}
+	putchar(new_line);
+	return (0);
+}
