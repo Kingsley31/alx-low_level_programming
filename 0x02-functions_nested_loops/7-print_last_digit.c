435c0624469f0844f9f377754ab082a6b9c72845
Kingsley <kingsleyemeka31@gmail.com>
2023-07-18 14:37:23 -0700

    Added 7-print_last_digit.c source file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,13 @@
  */
 int print_last_digit(int n)
 {
+	int last_digit = n % 10;
+
 	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-	}
-	int last_digit = n % 10;
+	}	
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
