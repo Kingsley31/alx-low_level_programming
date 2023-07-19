c2ba31c32c1521dc8f346f0429d1b2021998f7a9
Kingsley <kingsleyemeka31@gmail.com>
2023-07-18 17:45:54 -0700

    Added 10-add.c source file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+
+/**
+ * add - adds two numbers and return the value
+ *
+ * @a: first number
+ *
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
