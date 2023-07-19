fc5e58722376c8c5d6c438e3cea6e9ea8069c07f
Kingsley <kingsleyemeka31@gmail.com>
2023-07-19 03:58:37 -0700

    Added 102-fibonacci.c source file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints fabonacci series starting from 1 and 2 to 50
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int printed[51];
+	int counter = 1;
+
+	while (counter <= 50)
+	{
+		if (couter == 1 || counter == 2)
+		{
+			printed[couter - 1] = counter;
+			printf("%d", counter);
+		}
+		else if (counter > 2)
+		{
+			printed[counter - 1] = printed[counter - 2] + printed[counter - 3];
+			printf("%d", printed[counter - 2] + printed[counter - 3]);
+		}
+		if (counter < 50)
+		{
+			printf(", ");
+		}
+		else
+			printf("\n");
+		counter++;
+	}
+	return (0);
+}
