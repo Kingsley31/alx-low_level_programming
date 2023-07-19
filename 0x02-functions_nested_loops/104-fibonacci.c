30350c3fea28b82dc3492fb47e8c09a18217d280
Kingsley <kingsleyemeka31@gmail.com>
2023-07-19 05:52:34 -0700

    Added 104-fibonacci.c source file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,16 +9,16 @@
  */
 int main(void)
 {
-	uintmax_t prev1 = 1;
-	uintmax_t prev2 = 2;
+	static unsigned long int prev1 = 1;
+	static unsigned long int prev2 = 2;
 	int i;
 
-	printf("%ju, %ju", prev1, prev2);
+	printf("%lu, %lu", prev1, prev2);
 	for (i = 3; i <= 98; i++)
 	{
-		uintmax_t next = prev1 + prev2;
+		static unsigned long int next = prev1 + prev2;
 
-		printf(", %ju", next);
+		printf(", %lu", next);
 		prev1 = prev2;
 		prev2 = next;
 	}
