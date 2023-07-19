d8fd9fd0477871277e2a8fff0a10cff3dd0ddfe2
Kingsley <kingsleyemeka31@gmail.com>
2023-07-18 19:01:40 -0700

    Added 11-print_to_98.c source file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * print_whole_num - prints a whole number
+ *
+ * @num: the whole number to print
+ *
+ * Return: void
+ */
+void print_whole_num(int num)
+{
+	if (num > 9)
+	{
+		char nums[32];
+		int i = 0;
+
+		snprintf(nums, sizeof(nums), "%d", num);
+		while (i != '\0')
+		{
+			_putchar(nums[i]);
+			i++;
+		}
+	}
+	else if (num < 10)
+	{
+		_putchar(num + '0');
+	}
+}
+
+
+/**
+ * print_to_98 - print from n to 98
+ *
+ * @n: The number to print to 98
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i = n;
+
+	while (i <= 98)
+	{
+		if (i < 0)
+		{
+			int abs_i = i * -1;
+
+			_putchar('-');
+			print_whole_num(abs_i);
+		}
+		else if (i >= 0)
+			print_whole_num(i);
+		if (i < 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
