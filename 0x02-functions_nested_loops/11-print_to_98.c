335a823786d27b17ab775e1eea26a49ea84d322c
Kingsley <kingsleyemeka31@gmail.com>
2023-07-18 19:29:23 -0700

    Added 11-print_to_98.c source file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,26 +3,6 @@
 
 
 /**
- * print_whole_num - prints a whole number
- *
- * @num: the whole number to print
- *
- * Return: void
- */
-void print_whole_num(int num)
-{
-	if (num > 9)
-	{
-		printf("%d", num);
-	}
-	else if (num < 10)
-	{
-		_putchar(num + '0');
-	}
-}
-
-
-/**
  * print_to_98 - print from n to 98
  *
  * @n: The number to print to 98
@@ -31,25 +11,30 @@ void print_whole_num(int num)
  */
 void print_to_98(int n)
 {
-	int i = n;
-
-	while (i <= 98)
+	int i;
+	if (n <= 98)
 	{
-		if (i < 0)
+		for (i = n; i <= 98; i++)
 		{
-			int abs_i = i * -1;
-
-			_putchar('-');
-			print_whole_num(abs_i);
+			printf("%d", i);
+			if (i < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-		else if (i >= 0)
-			print_whole_num(i);
-		if (i < 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		i+=1;
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 }
