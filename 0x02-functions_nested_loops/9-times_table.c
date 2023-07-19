ed99bce54efaf0b9892d0f68510400df2cb6afd2
Kingsley <kingsleyemeka31@gmail.com>
2023-07-18 17:07:40 -0700

    Added 9-times_table.c source file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int row = 0;
+
+	while (row <= 9)
+	{
+		int col = 0;
+
+		while (col <= 9)
+		{
+			int num = row * col;
+
+			if (num > 9)
+			{
+				_putchar((num/10) + '0');
+				_putchar((num%10) + '0');
+			}
+			else if (num < 10)
+				_putchar(num + '0');
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			if (col == 9)
+				_putchar('\n');
+			col++;
+		}
+		row++;
+	}
+}
