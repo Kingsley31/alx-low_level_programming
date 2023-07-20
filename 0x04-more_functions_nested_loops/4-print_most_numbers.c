392a54bc6d5b4e5645445e5de0d536e7dbac08b1
Kingsley <kingsleyemeka31@gmail.com>
2023-07-20 15:02:26 -0700

    Added 4-print_most_numbers.c source file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
-
 /**
- * print_most_numbers - prints numbers from 0 to 9 skipping 2 and 4
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * @void: Takes nothing
  *
- * @void: takes nothing
- *
- * Return: returns nothing
+ * Return: Returns nothing
  */
-
 void print_most_numbers(void)
 {
-	char i = '0';
+	char i;
 
-	while (i <= '9')
+	for (i = '0'; i <= '9'; i++)
 	{
 		if (i == '2' || i == '4')
 			continue;
 		_putchar(i);
-		i++;
 	}
 	_putchar('\n');
 }
