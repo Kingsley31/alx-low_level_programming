9fc4042a4e8ae5d9aa14c2b409d3fdcb320e493c
Kingsley <kingsleyemeka31@gmail.com>
2023-07-23 06:58:08 -0700

    Modified 100-prime_factor.c source file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,6 +10,7 @@ int is_prime(unsigned long int n)
 	unsigned long int counter = 5;
 	unsigned long int number_is_prime = 1;
 	
+	
 	if (n < 2)
 		return (0);
 	if (n == 2 || n == 3)
@@ -39,7 +40,8 @@ int main(void)
 	unsigned long int largest_factor = 0;
 	int assumed_factor_is_prime = 0;
 	unsigned long int prime_factors_sum = 1;
-	
+
+
 	while (prime_factors_sum < n)
 	{
 		if (dividend % asummed_factor != 0)
