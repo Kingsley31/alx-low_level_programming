7083e8d0bee25798b4cc0b920b042280d0a3a027
Kingsley <kingsleyemeka31@gmail.com>
2023-07-23 06:20:25 -0700

    Added 100-prime_factor.c source file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,23 +7,20 @@
  */
 int is_prime(unsigned long int n)
 {
-	unsigned long int counter = 2;
+	unsigned long int counter = 5;
 	unsigned long int number_is_prime = 1;
 	
-	if (n <= 1)
-	{
+	if (n < 2)
 		return (0);
-	}
-	else if (n <= 3)
-	{
+	if (n == 2 || n == 3)
 		return (number_is_prime);
-	}
-	for (counter = 2; counter < n; counter++)
+	if (n % 2 == 0 || n % 3 == 0)
+		return (0);
+	for (counter = 5; counter * counter < n; counter += 6)
 	{
 		if (n % counter == 0)
 		{
-			number_is_prime = 0;
-			break;
+			return (0);
 		}
 	}
 	return (number_is_prime);
@@ -38,18 +35,27 @@ int is_prime(unsigned long int n)
 int main(void)
 {
 	unsigned long int n = 612852475143;
-	unsigned long int i;
+	unsigned long int asummed_factor = 2;
+	unsigned long int dividend = n;
 	unsigned long int largest_factor = 0;
-	int prime = 0;
-	
-	for (i = 2; i <= n; i++)
-	{
-		if (n % i != 0)
-			continue;
-		prime = is_prime(i);
+	int assumed_factor_is_prime = 0;
+	unsigned long int prime_factors_sum = 1;
 
-		if (prime == 1 && i > largest_factor)
-			largest_factor = i;
+	while (prime_factors_sum < n)
+	{
+		if (dividend % asummed_factor != 0)
+		{
+			asummed_factor++;
+			continue;
+		}
+		assumed_factor_is_prime = is_prime(asummed_factor);
+		if (assumed_factor_is_prime == 1 && asummed_factor > largest_factor)
+			largest_factor = asummed_factor;
+		if (assumed_factor_is_prime == 1)
+		{
+			prime_factors_sum *= asummed_factor;
+			dividend /= asummed_factor;
+		}
 	}
 	printf("%lu\n", largest_factor);
 	return (0);
