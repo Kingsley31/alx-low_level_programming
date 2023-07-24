76a0c00ab195cda8e6af48ce4bdd735d4aecae7f
Kingsley <kingsleyemeka31@gmail.com>
2023-07-24 16:06:53 -0700

    Added 0-reset_to_98.c source file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+
+/**
+ * reset_to_98 - resets the value of a variable passed in to 98
+ * @n: pointer to the variables address
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains solutions to 0x05-pointers_arrays_strings project tasks
