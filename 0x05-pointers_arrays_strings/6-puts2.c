6fe84d3cfc6be255556d12aa32e2944689ac29b6
Kingsley <kingsleyemeka31@gmail.com>
2023-07-25 09:05:43 -0700

    Added 6-puts2.c source file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * puts2 - prints characters of a string but conseqeutively skipping
+ * the next character in line
+ *
+ * @str: the string to print
+ * Return:void
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (*(str + count) != 0)
+	{
+		_putchar(str[count]);
+		count += 2;
+	}
+	_putchar('\n');
+}
