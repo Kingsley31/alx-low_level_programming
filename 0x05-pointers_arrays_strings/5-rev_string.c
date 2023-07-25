a4818d2b0fe7cba49bec67e0615120bbc3b9926a
Kingsley <kingsleyemeka31@gmail.com>
2023-07-25 08:37:16 -0700

    Added 5-rev_string.c surce file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+
+/**
+ * rev_string - reverses a string variable contents
+ * @s: pointer to first character of the string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int count = 0;
+	int end = 0;
+	
+	if (*(s + len) == 0)
+		return;
+	while (*(s + len) != 0)
+	{
+		len++;
+	}
+	len -= 1;
+	while (count < len / 2)
+	{
+		int upper_index = len - count;
+		int lower_index = count;
+		
+		if (upper_index > 0)
+		{
+			char tmp = *(s + upper_index);
+			s[upper_index] = *(s + lower_index);
+			s[lower_index] = tmp;
+		}
+		count++;
+	}
+}
