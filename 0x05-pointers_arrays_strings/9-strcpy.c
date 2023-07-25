ec3fb0c15669c1844f6a5659dc8dc432a9f5dba8
Kingsley <kingsleyemeka31@gmail.com>
2023-07-25 10:40:34 -0700

    Added 9-strcpy.c source file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+
+/**
+ * _strcpy - copy the string in the source to destination and
+ * return destination
+ *
+ * @dest: the destination to copy string to
+ * @src: the source of the string to be copied
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = &src;
+	return (*dest);
+}
