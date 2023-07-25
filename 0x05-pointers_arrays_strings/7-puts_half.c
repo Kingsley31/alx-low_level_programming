72f753f3bc0dfe1e24b8c881ea14ce2a0d5dac52
Kingsley <kingsleyemeka31@gmail.com>
2023-07-25 09:41:38 -0700

    Added 7-puts_half.c source file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string characters
+ * @str: string to print the second half characters
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int count = 0;
+
+	while (*(str + len) != 0)
+	{
+		len++;
+	}
+	len -= 1;
+	count = len / 2;
+	if (len % 2 != 0)
+	{
+		count = (len - 1) / 2;
+	}
+	while (*(str + count) != 0)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+}
