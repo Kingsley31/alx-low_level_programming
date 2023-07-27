7a41292f968443e847a66d247f75930bc41a5bd3
Kingsley <kingsleyemeka31@gmail.com>
2023-07-27 14:18:22 -0700

    Added 0-strcat.c source file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,11 +3,10 @@
  * _strcat - concatenates two strings
  * @dest: thr first string that the second string should be joined
  * together with
- *
  * @src: the second string to be added to the dest string
- *
  * Return: char* pointer
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int len = 0;
