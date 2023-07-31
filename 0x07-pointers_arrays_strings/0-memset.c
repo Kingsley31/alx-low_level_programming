c24ea757bf4ba3f8f845cbd17cf50af1797c5595
Kingsley <kingsleyemeka31@gmail.com>
2023-07-31 13:45:35 -0700

    Added 0-memset.c source file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+
+/**
+ * _memset - fills memory with constant n bytes
+ * @s: pointer to memory
+ * @b: the constant byte to fill the memory with
+ * @n: number of s memory areas to fill
+ * Return: returns pointer to memory s
+ */
+char *_memset(char *s,char b,int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		(s + i) = b;
+		i++;
+	}
+	return s;
+}
