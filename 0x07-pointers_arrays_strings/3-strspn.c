07db040e7b088bc294300151aa7863e63dcd4bc0
Kingsley <kingsleyemeka31@gmail.com>
2023-08-01 09:48:09 -0700

    Added 3-strspn.c source file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,13 +8,18 @@
 int _strspn(char *s, char *accept)
 {
 	int i = 0;
+	int j = 0;
 	int count = 0;
 
 	while (s[i] >= '\0')
 	{
-		if (s[i] == accept[i])
+		while (accept[i] != '\0')
 		{
-			count++;
+			if (s[i] == accept[i])
+			{
+				count++;
+			}
+			j++;
 		}
 		i++;
 	}
