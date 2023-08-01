321cac1629f2162590a6fa257f40fe986b6bb8e5
Kingsley <kingsleyemeka31@gmail.com>
2023-08-01 08:08:45 -0700

    Added 1-memcpy.c source file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,8 @@
 
 /**
  * _memcpy - copies memory area of src to dest
- * @dest - area to be copied into
- * @src - area to be copied from
+ * @dest: area to be copied into
+ * @src: area to be copied from
  * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
