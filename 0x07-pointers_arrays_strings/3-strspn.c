f7d6c7fa09ce04f4bbf9e1746d2234a24326753f
Kingsley <kingsleyemeka31@gmail.com>
2023-08-01 09:25:57 -0700

    Added 3-strspn.c source file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+
+/**
+ * _strspn - gets the length of a prefix string
+ * @s: string to compare with accept
+ * @accept: string to check its byte occurance in s
+ * Return: length of accept bytes in s
+ */
+int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int count = 0;
+
+	while (s[i] >= '\0')
+	{
+		if (s[i] == accept[i])
+		{
+			count++;
+		}
+		i++;
+	}
+	return count;
+}
