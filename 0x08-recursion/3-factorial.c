a82d1015bc97955eb6d45ebbb7b5d890614b3752
Kingsley <kingsleyemeka31@gmail.com>
2023-08-02 15:03:43 -0700

    Added 3-factorial.c source file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+
+/**
+ * factorial - return factorial of a number
+ * @n: number that it's factorial should be calculated
+ * Return: 0
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return 1;
+	if (n < 0)
+		return -1;
+	return (n * factorial(n - 1));
+}
