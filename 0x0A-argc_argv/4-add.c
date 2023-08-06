724b50ec6497da44211ff489d25c253811f213dd
Kingsley <kingsleyemeka31@gmail.com>
2023-08-06 04:30:28 -0700

    Added 4-add.c source file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,42 @@
 #include <stdlib.h>
 
 /**
+ * _isdigit - Checks if a character is between 0 and 9
+ * @c: The character to check
+ * Return: 1 if c is betwen 0 and 9 and 0 if not
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+/**
+ * str_is_int - checks if a string is int
+ * @s: string to check
+ * Return: 0 if s is not int, 1 if s is int
+ */
+int str_is_int(char *s)
+{
+	int i = 0;
+
+	if (!_isdigit(s[i]) || s[i] != '-')
+	{
+		return (0);
+	}
+	i++;
+	while (s[i] != '\0')
+	{
+		if (!_isdigit(s[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
  * main - sums positive numbers supplied as arguements
  * @argc: arguement count
  * @argv: arguement array
@@ -19,7 +55,7 @@ int main(int argc, char *argv[])
 	}
 	while (i < argc)
 	{
-		if (atoi(argv[i]) == 0)
+		if (str_is_int(argv[i]) == 0)
 		{
 			printf("Error\n");
 			return (1);
