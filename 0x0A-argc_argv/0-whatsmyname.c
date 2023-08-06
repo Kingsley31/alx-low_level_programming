90967ed2191286a416b1a4cfd9eb0ec95ca8abbf
Kingsley <kingsleyemeka31@gmail.com>
2023-08-06 02:10:40 -0700

    Added 0-whatsmyname.c source file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ * @argc - arguement count
+ * @argv - arguement ventor, contains array of arguements passed to the
+ * program.
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
