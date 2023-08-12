bb9899cc46c40aac98166f47e536d782a2e59a60
Kingsley <kingsleyemeka31@gmail.com>
2023-08-12 05:01:45 -0700

    Added 2-calloc.c source file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,24 @@
 #include <stdlib.h>
 
 /**
+ * initmem - initializes memories to zero
+ * @n: number of memory blocks
+ * @p: pointer to the memory blocks
+ * Return: void
+ */
+void initmem(void *p, unsigned int n)
+{
+	unsigned char *ptr = (unsigned char *) p;
+	unsigned int i = 0;
+
+	while (i < n)
+        {
+                ptr[i] = 0;
+                i++;
+        }
+}
+
+/**
  * _calloc - allocates memory for an array using malloc
  * @nmemb: number of blocks to allocate
  * @size: size of the array elements data type
@@ -9,17 +27,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
-	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	while (i < nmemb)
-	{
-		p[i] = 0;
-		i++;
-	}
+	initmem(p, nmemb);
 	return (p);
 }
