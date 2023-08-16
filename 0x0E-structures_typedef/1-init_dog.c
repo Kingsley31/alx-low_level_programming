e4dff273d1fca4df3387ed6e9b13988d6aa60958
Kingsley <kingsleyemeka31@gmail.com>
2023-08-16 03:51:42 -0700

    Added 1-init_dog.c source file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+
+/**
+ * init_dog - initializes dog data structure
+ * @d: the dog data structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
