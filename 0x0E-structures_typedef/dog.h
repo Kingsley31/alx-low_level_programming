14cb80f09ff2a383a6b05558935a42d38656dd43
Kingsley <kingsleyemeka31@gmail.com>
2023-08-16 03:35:27 -0700

    Added dog.h header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struc dog - create a dog structure
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: a dog data structure with the name, age and owner's name
+ */
 struct dog
 {
 	char *name;
