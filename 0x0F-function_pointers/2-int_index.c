990fb4af41ec87255db298718101da7b15df1d66
Kingsley <kingsleyemeka31@gmail.com>
2023-08-17 15:28:00 -0700

    Added 2-int_index.c source file

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * init_index - searches for an integer
+ * @array: array containing the integer to be search for
+ * @size: number of elements in the array
+ * @cmp: the pointer to the comparator function
+ * Return: the index of the first element of which cmp doesn't return 0
+ */
+int init_index(int *array, int size, int (*cmp)(int))
+{
+	int len = size;
+	int i = 0;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+	       return (-1);
+	while (i < len)
+	{
+		if (cmp(array[i]))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
