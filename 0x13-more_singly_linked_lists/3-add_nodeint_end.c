bf5714abcfc5e2ea81b58a36de01ce37ed04c3b6
Kingsley <kingsleyemeka31@gmail.com>
2023-08-29 06:57:51 -0700

    Added 3-add_nodeint_end.c source file

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,10 +21,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while (*ptr->next != NULL)
+	while ((*ptr)->next != NULL)
 	{
-		*ptr = *ptr->next;
+		*ptr = (*ptr)->next;
 	}
-	*ptr->next = new_node;
+	(*ptr)->next = new_node;
 	return (new_node);
 }
