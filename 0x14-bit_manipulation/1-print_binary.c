80ca8bebf2c2fcb45a6bc0c005c9426a71864005
Kingsley <kingsleyemeka31@gmail.com>
2023-09-09 14:22:15 -0700

    Added 1-print_binary.cv source file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,9 +9,9 @@ void print_binary(unsigned long int n)
 {
 	if (n <= 1)
 	{
-		printf("%d", n);
+		printf("%ld", n);
 		return;
 	}
 	print_binary(n >> 1);
-	printf("%d", (n & 1));
+	printf("%ld", (n & 1));
 }
