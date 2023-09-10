ccb552d1c22015a352193fc6144ed7e238034f4f
Kingsley <kingsleyemeka31@gmail.com>
2023-09-09 18:18:43 -0700

    Added 4-clear_bit.c  source file

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -32,7 +32,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 		return (0);
 	if (*n == 0 && index == 0)
 		return (1);
-	if (!bit_is_set(*n, index) == 1)
+	if ((!bit_is_set(*n, index)) == 1)
 		return (status);
 	*n = *n ^ (1 << index);
 	status = 1;
