8010f5a553e3c17903a95e41d8199a952cd978d2
Kingsley <kingsleyemeka31@gmail.com>
2023-09-15 15:47:24 -0700

    Added 0-read_textfile.c source file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
 /**
+ * close_and_free - close file and free pointer
+ * @fd: file descriptor of file to close
+ * @ptr: pointer to free
+ * Return: void
+ */
+void close_and_free(int fd, char *ptr)
+{
+	close(fd);
+	free(ptr);
+}
+
+/**
  * read_textfile - reads a text file and prints it to the POSIX standard
  * output.
  * @filename: filename to read
@@ -9,25 +21,41 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int bytes_written = 0;
-	char *buffer = malloc(letters + 1);
+	int fd, bytes_written = 0;
+	char *buffer;
 
-	if (buffer == NULL)
+	if (letters < 1)
+	{
 		return (0);
+	}
 	if (filename == NULL || (*filename) == '\0')
 	{
 		return (0);
 	}
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
+	{
 		return (0);
+	}
+	buffer = malloc(letters + 1);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 	bytes_written = read(fd, buffer, letters);
 	if (bytes_written < 0)
+	{
+		close_and_free(fd, buffer);
 		return (0);
+	}
 	buffer[letters] = '\0';
 	bytes_written = write(1, buffer, letters);
 	if (bytes_written < 0)
+	{
+		close_and_free(fd, buffer);
 		return (0);
+	}
+	close_and_free(fd, buffer);
 	return (bytes_written);
 }
