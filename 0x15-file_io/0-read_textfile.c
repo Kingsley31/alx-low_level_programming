3770b97fe723c43dc20a6538a782b1145d992922
Kingsley <kingsleyemeka31@gmail.com>
2023-09-15 16:18:55 -0700

    Added 0-read_textfile.c source file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -51,7 +51,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	buffer[letters] = '\0';
-	bytes_written = write(1, buffer, bytes_written);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_written);
 	if (bytes_written < 0)
 	{
 		close_and_free(fd, buffer);
