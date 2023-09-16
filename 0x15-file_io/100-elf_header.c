7a17a513edab378d83a2608dc64afd8dac6a7b0f
Kingsley <kingsleyemeka31@gmail.com>
2023-09-15 17:19:12 -0700

    Added 100-elf_header.c source file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,9 +8,59 @@
  * print_elf_header_info - Print information from the ELF header.
  * @header: Pointer to the ELF header structure.
  */
+void print_elf_header_info(Elf64_Ehdr *header);
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ * Return: 0 on success, 98 on error.
+ */
+int main(int argc, char *argv[]) {
+    int fd;
+    Elf64_Ehdr header;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        return 98;
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        return 98;
+    }
+
+    if (read(fd, &header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr)) {
+        fprintf(stderr, "Error reading ELF header\n");
+        close(fd);
+        return 98;
+    }
+
+    if (header.e_ident[EI_MAG0] != ELFMAG0 || 
+        header.e_ident[EI_MAG1] != ELFMAG1 ||
+        header.e_ident[EI_MAG2] != ELFMAG2 ||
+        header.e_ident[EI_MAG3] != ELFMAG3) {
+        fprintf(stderr, "Not an ELF file\n");
+        close(fd);
+        return 98;
+    }
+
+    print_elf_header_info(&header);
+
+    close(fd);
+    return 0;
+}
+
+/**
+ * print_elf_header_info - Print information from the ELF header.
+ * @header: Pointer to the ELF header structure.
+ */
 void print_elf_header_info(Elf64_Ehdr *header) {
+    int i;
+
     printf("Magic:   ");
-    for (int i = 0; i < EI_NIDENT; i++) {
+    for (i = 0; i < EI_NIDENT; i++) {
         printf("%02x ", header->e_ident[i]);
     }
     printf("\n");
@@ -44,42 +94,3 @@ void print_elf_header_info(Elf64_Ehdr *header) {
     printf("Entry point address: 0x%lx\n", header->e_entry);
 }
 
-/**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
- * Return: 0 on success, 98 on error.
- */
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-        return 98;
-    }
-
-    int fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
-        perror("Error opening file");
-        return 98;
-    }
-
-    Elf64_Ehdr header;
-    if (read(fd, &header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr)) {
-        fprintf(stderr, "Error reading ELF header\n");
-        close(fd);
-        return 98;
-    }
-
-    if (header.e_ident[EI_MAG0] != ELFMAG0 || 
-        header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 ||
-        header.e_ident[EI_MAG3] != ELFMAG3) {
-        fprintf(stderr, "Not an ELF file\n");
-        close(fd);
-        return 98;
-    }
-
-    print_elf_header_info(&header);
-
-    close(fd);
-    return 0;
-}
