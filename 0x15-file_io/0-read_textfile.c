f3064154b4512740d771d5384b341bf556df9bb2
Kingsley31 <kingsleyemeka31@gmail.com>
2023-10-30 19:29:34 +0100

    Completed file io project

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * close_and_free - close file and free pointer
+ * @fd: file descriptor of file to close
+ * @ptr: pointer to free
+ * Return: void
+ */
+void close_and_free(int fd, char *ptr)
+{
+	close(fd);
+	free(ptr);
+}
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard
+ * output.
+ * @filename: filename to read
+ * @letters: number of letters to read.
+ * Return: number of letters it could read and print or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_written = 0;
+	char *buffer;
+
+	if (letters < 1)
+	{
+		return (0);
+	}
+	if (filename == NULL || (*filename) == '\0')
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	bytes_written = read(fd, buffer, letters);
+	if (bytes_written < 0)
+	{
+		close_and_free(fd, buffer);
+		return (0);
+	}
+	buffer[letters] = '\0';
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_written);
+	if (bytes_written < 0)
+	{
+		close_and_free(fd, buffer);
+		return (0);
+	}
+	close_and_free(fd, buffer);
+	return (bytes_written);
+}
---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
+
+/**
+ * print_elf_header_info - Print information from the ELF header.
+ * @header: Pointer to the ELF header structure.
+ */
+void print_elf_header_info(Elf64_Ehdr *header);
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ * Return: 0 on success, 98 on error.
+ */
+int main(int argc, char *argv[]) {
+    int fd;
+    Elf64_Ehdr header;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        return 98;
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        return 98;
+    }
+
+    if (read(fd, &header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr)) {
+        fprintf(stderr, "Error reading ELF header\n");
+        close(fd);
+        return 98;
+    }
+
+    if (header.e_ident[EI_MAG0] != ELFMAG0 || 
+        header.e_ident[EI_MAG1] != ELFMAG1 ||
+        header.e_ident[EI_MAG2] != ELFMAG2 ||
+        header.e_ident[EI_MAG3] != ELFMAG3) {
+        fprintf(stderr, "Not an ELF file\n");
+        close(fd);
+        return 98;
+    }
+
+    print_elf_header_info(&header);
+
+    close(fd);
+    return 0;
+}
+
+/**
+ * print_elf_header_info - Print information from the ELF header.
+ * @header: Pointer to the ELF header structure.
+ */
+void print_elf_header_info(Elf64_Ehdr *header) {
+    int i;
+
+    printf("Magic:   ");
+    for (i = 0; i < EI_NIDENT; i++) {
+        printf("%02x ", header->e_ident[i]);
+    }
+    printf("\n");
+    
+    printf("Class:   %d-bit\n", (header->e_ident[EI_CLASS] == ELFCLASS32) ? 32 : 64);
+    
+    printf("Data:    %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
+    
+    printf("Version: %d (current)\n", header->e_ident[EI_VERSION]);
+    
+    printf("OS/ABI:  ");
+    switch (header->e_ident[EI_OSABI]) {
+        case ELFOSABI_SYSV: printf("UNIX System V ABI\n"); break;
+        case ELFOSABI_HPUX: printf("HP-UX ABI\n"); break;
+        case ELFOSABI_STANDALONE: printf("Standalone ABI\n"); break;
+        default: printf("Unknown ABI\n");
+    }
+    
+    printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
+    
+    printf("Type:    ");
+    switch (header->e_type) {
+        case ET_NONE: printf("NONE (No file type)\n"); break;
+        case ET_REL: printf("REL (Relocatable file)\n"); break;
+        case ET_EXEC: printf("EXEC (Executable file)\n"); break;
+        case ET_DYN: printf("DYN (Shared object file)\n"); break;
+        case ET_CORE: printf("CORE (Core file)\n"); break;
+        default: printf("Unknown\n");
+    }
+    
+    printf("Entry point address: 0x%lx\n", header->e_entry);
+}
+
